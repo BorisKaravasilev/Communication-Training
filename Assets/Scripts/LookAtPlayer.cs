fc778b0f229c76d8dc7368d2162fbd79ee88a03f
Magnus Arnild Ipsen <102797036+kompasbank-magnus@users.noreply.github.com>
2022-12-07 11:05:36 +0100

    update lookatplayer

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LookAtPlayer : MonoBehaviour
 {
     private Transform player;
+    private float speed = 5f;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,9 @@ public class LookAtPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(transform);
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
     }
 }
